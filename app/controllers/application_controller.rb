b54ad7b61301975fdb0a8af4d6fc69e76db2a91e
fantasticlife <michael@smethur.st>
2026-09-02 13:46:50 +0100

    added a method to generate csv headers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
   include LibraryDesign::Crumbs
   
   $SITE_TITLE = 'Constituency Responsibilities'
+  
+private
+
+  #def csvify_title( csv_title )
+    #csv_title.downcase.gsub( ' ', '-' )
+  #end
+  
+  def csv_response_headers( title )
+    title = title.downcase.gsub( ' ', '-' )
+    response.headers['Content-Disposition'] = "attachment; filename=\"#{title}.csv\""
+  end
 end
